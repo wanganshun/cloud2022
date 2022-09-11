703a5cd8aeac3f78e4639beea6cfbb2058fbb0e6
wanganshun <2635550416@qq.com>
2022-09-11 14:45:57 +0800

    parent project create

---
+++ src/main/java/Main.java
@@ -0,0 +1,10 @@
+/**
+ * @program: cloud2022
+ * @author: WangAnShun
+ * @create: 2022-09-11 14:28
+ **/
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
